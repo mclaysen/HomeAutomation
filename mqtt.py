f89d8807744e3d6ca71113a701510fd4f3fa6402
Matt Claysen <me@mclaysen.com>
2022-03-12 09:15:06 -0500

    add in rtl

--- mqtt.py
+++ mqtt.py
@@ -1,10 +1,16 @@
 from turtle import home
+import json
 import paho.mqtt.client as paho
 
 def on_message_dte(client, userdata, message):
-    print("received message =",str(message.payload.decode("utf-8")))
     powerData = message.payload.decode("utf-8")
     homeassistantclient.publish("energy/meter",powerData, retain= True)
+
+def on_message_rtl(client, userdata, message):
+    tempData = message.payload.decode("utf-8")
+    decodedTempData =json.loads(tempData)
+    if decodedTempData["id"]==5161:
+        homeassistantclient.publish("rtl_433/basement",message.payload)#publish
 
 def connect_homeassistant(clientid):
     homeassistantip="192.168.86.78"
@@ -20,8 +26,20 @@ def connect_dte(clientid):
     client.connect(dte_ip, 2883)
     return client
 
+def connect_rtl(clientid):
+    rtl_ip = "localhost"
+    client = paho.Client(clientid)
+    client.connect(rtl_ip, 1883)
+    return client
+
 homeassistantclient = connect_homeassistant("client-1")
-rtlclient = connect_dte("client-1")
-rtlclient.on_message=on_message_dte
-rtlclient.subscribe("event/metering/#")
+
+dteclient = connect_dte("client-1")
+dteclient.on_message=on_message_dte
+dteclient.subscribe("event/metering/#")
+dteclient.loop_start()
+
+rtlclient = connect_rtl("client-1")
+rtlclient.on_message = on_message_rtl
+rtlclient.subscribe("rtl_433/localhost/events")
 rtlclient.loop_forever()
