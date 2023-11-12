d1f04fe6e5fba54dd755fa77534ef024f02f9db2
Matthew Claysen <me@mclaysen.com>
2023-11-11 22:55:41 -0500

    migrate to configs

--- mqtt.py
+++ mqtt.py
@@ -1,5 +1,14 @@
 import json
 import paho.mqtt.client as paho
+import signal
+import sys
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+with open('config.json') as f:
+    appsettings = json.load(f)
 
 def on_message_dte(client, userdata, message):
     powerData = message.payload.decode("utf-8")
@@ -27,30 +36,47 @@ def connect_homeassistant(clientid):
     homeassistantip="192.168.86.78"
     print("connecting to home assistant", homeassistantip)
     homeassistantclient = paho.Client(clientid)
-    homeassistantclient.username_pw_set("mqtt","snapple15")
+    username = config.get('HOMEASSISTANT', 'USER')
+    password = config.get('HOMEASSISTANT', 'PASSWORD')
+    homeassistantclient.username_pw_set(username=username,password=password)
     homeassistantclient.connect(homeassistantip)
     return homeassistantclient
 
 def connect_dte(clientid):
     client= paho.Client(clientid)
-    dte_ip="192.168.2.190"
+    dte_ip = appsettings.get("DTE_IP")
     client.connect(dte_ip, 2883)
     return client
 
 def connect_rtl(clientid):
-    rtl_ip = "192.168.86.211"
+    rtl_ip = appsettings.get("RTL_IP")
     client = paho.Client(clientid)
     client.connect(rtl_ip, 1883)
     return client
 
-homeassistantclient = connect_homeassistant("client-0")
+homeassistantclient = connect_homeassistant("client-5")
 
-dteclient = connect_dte("client-1")
+dteclient = connect_dte("client-7")
 dteclient.on_message=on_message_dte
 dteclient.subscribe("event/metering/#")
 dteclient.loop_start()
 
-rtlclient = connect_rtl("client-2")
+rtlclient = connect_rtl("client-8")
 rtlclient.on_message = on_message_rtl
 rtlclient.subscribe("rtl_433/raspberrypi/events/#")
 rtlclient.loop_forever()
+
+def exit_gracefully(signum, frame):
+    print("exiting")
+    homeassistantclient.disconnect()
+    dteclient.disconnect()
+    dteclient.loop_stop()
+
+    rtlclient.disconnect()
+    rtlclient.loop_stop()
+
+    sys.exit(0)
+
+signal.signal(signal.SIGTERM, exit_gracefully)
+signal.signal(signal.SIGINT, exit_gracefully)
+signal.signal(signal.SIGQUIT, exit_gracefully)
