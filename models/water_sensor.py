0eb85667f7d8a42bac6268d2999fc7b103a9339d
Matthew Claysen <me@mclaysen.com>
2026-04-08 23:20:28 -0400

    update govee sensor

--- models/water_sensor.py
+++ models/water_sensor.py
@@ -1,12 +1,16 @@
 class WaterSensorData:
-    def __init__(self, time: str, model: str, id: int, event: str, detect_wet: bool, code: str, mic: str) -> None:
+    def __init__(self, time: str, model: str, id: int, event: str, code: str, mic: str, detect_wet: bool | None = None, battery_ok: float | None = None, battery_mV: int | None = None, leak_num: int | None = None) -> None:
         self.time = time
         self.model = model
         self.id = id
         self.event = event
-        self.detect_wet = detect_wet
         self.code = code
         self.mic = mic
+        self.detect_wet = detect_wet
+        self.battery_ok = battery_ok
+        self.battery_mV = battery_mV
+        self.leak_num = leak_num
+
 
     @classmethod
     def from_dict(cls, data: dict) -> 'WaterSensorData':
