14b32d4ff71f14c682372f2858900a6bf583a467
Matthew Claysen <me@mclaysen.com>
2026-04-13 00:22:29 -0400

    enable discovery for the door sensors

--- mqtt.py
+++ mqtt.py
@@ -52,12 +52,19 @@ def connect_homeassistant() -> MqttPublisher:
     return client
 
 def publish_discovery(client, appSettings : Config) -> None:
-    discoveryFactory = DiscoveryFactory(SensorType.TEMP_SENSOR)
+    tempDiscoveryFactory = DiscoveryFactory(SensorType.TEMP_SENSOR)
+    doorDiscoveryFactory = DiscoveryFactory(SensorType.DOOR_SENSOR)
 
     for modelMapping in appSettings.ModelMappings:
         if(modelMapping.sensorType == SensorType.TEMP_SENSOR):
             for sensor in modelMapping.sensors:
-                discoveryObject = discoveryFactory.getDiscoveryObject(sensor.name, str(sensor.id))
+                discoveryObject = tempDiscoveryFactory.getDiscoveryObject(sensor.name, str(sensor.id), None)
+                discoveryTopic = discoveryObject.topic_for_discovery()
+                discoveryPayload = discoveryObject.getDiscoveryPayload("rtl_433/"+sensor.name)
+                client.publish(discoveryTopic, json.dumps(discoveryPayload), 1, True)
+        elif(modelMapping.sensorType == SensorType.DOOR_SENSOR):
+            for sensor in modelMapping.sensors:
+                discoveryObject = doorDiscoveryFactory.getDiscoveryObject(sensor.name, str(sensor.id), modelMapping.model)
                 discoveryTopic = discoveryObject.topic_for_discovery()
                 discoveryPayload = discoveryObject.getDiscoveryPayload("rtl_433/"+sensor.name)
                 client.publish(discoveryTopic, json.dumps(discoveryPayload), 1, True)
