17522f2c8f4e8fa87f0e188c374e373eec699860
Matthew Claysen <me@mclaysen.com>
2026-04-21 23:12:57 -0400

    add in the energy handler

--- models/deviceType.py
+++ models/deviceType.py
@@ -2,5 +2,5 @@ import enum
 
 class DeviceType(enum.Enum):
     RF_433 = 1
-    DTE_METER = 2
+    ENERGY_METER = 2
     HOME_ASSISTANT = 3
---
+++ mqttHandlers/messageHandlers/energyMessageHandler.py
@@ -0,0 +1,26 @@
+import logging
+from models.sensorMappings import Config
+from mqttHandlers.subscriberModel import Subscriber
+from typing import TypeVar
+from mqttHandlers.publisher import MqttPublisher
+from models.energyType import EnergyType
+import json
+
+T = TypeVar('T')
+
+class EnergyMessageHandler:
+    def __init__(self, subscriberData: Subscriber,  appSettings: Config,  publisher: MqttPublisher, logger: logging.Logger):
+        self.appSettings = appSettings
+        self.publisher = publisher
+        self.logger = logger
+        self.deviceType = subscriberData.deviceType
+
+    def on_message(self, payload: T) -> None:
+        self.logger.debug(payload)
+        try:
+            if payload.type == EnergyType.INSTANT:
+                self.publisher.publish("energy/meter/instant",json.dumps(payload.to_dict()), 0, False)
+            else:
+                self.publisher.publish("energy/meter/summary",json.dumps(payload.to_dict()), 0, False)
+        except Exception as e:
+            self.logger.error("Error parsing payload for DTE. Exception: %s", e)
