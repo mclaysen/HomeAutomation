565356f30f4cef5106e0328cf969e6fca9645e2b
Matthew Claysen <me@mclaysen.com>
2026-04-21 23:13:14 -0400

    add in the hass message handler

---
+++ mqttHandlers/messageHandlers/homeAssistantHandler.py
@@ -0,0 +1,29 @@
+import json
+import logging
+from models.sensorMappings import Config
+from mqttHandlers.subscriberModel import Subscriber
+from typing import TypeVar
+from mqttHandlers.publisher import MqttPublisher
+
+T = TypeVar('T')
+
+class HomeAssistantMessageHandler:
+    def __init__(self, subscriberData: Subscriber,  appSettings: Config,  publisher: MqttPublisher, logger: logging.Logger):
+        self.appSettings = appSettings
+        self.publisher = publisher
+        self.logger = logger
+        self.deviceType = subscriberData.deviceType
+        
+    
+    def on_message(self, payload: T) -> None:
+        self.logger.debug(payload)
+        try:
+            tempModel = next(model for model in self.appSettings.ModelMappings if model.model == payload.model)
+            if(tempModel is not None):
+                sensor = next(sensor for sensor in tempModel.sensors if sensor.id == payload.id)
+                if(sensor is not None):
+                    self.publisher.publish("rtl_433/"+sensor.name, json.dumps(payload), 0, False)
+                else:
+                    self.logger.warning("No sensor found for %s", payload.id)
+        except Exception as e:
+            self.logger.error("Error handling message: %s", e)
